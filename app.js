208db8b1f58f994ce11bd061cfb1d189089f34f7
xingoxu <xingoxu@gmail.com>
2019-11-02 18:12:08 +0900

    support node 12

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 var express = require('express');
-var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
 
 var app = express();
 app.get('env') === 'production' ? app.disable('x-powered-by') : false;
@@ -10,8 +8,8 @@ app.get('env') === 'production' ? app.disable('x-powered-by') : false;
 // app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'ejs');
 app.get('env') !== 'production' ? (app.use(logger('dev'))) : false;
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
 
